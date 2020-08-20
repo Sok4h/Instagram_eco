a3cfa9da6355a49f7a5989ab04ab26c534c8c1bf
Sok4h <55671660+Sok4h@users.noreply.github.com>
2020-08-20 12:48:06 -0500

    cambio color texto

--- app/src/main/java/com/sokah/instagram/MainActivity.java
+++ app/src/main/java/com/sokah/instagram/MainActivity.java
@@ -3,12 +3,32 @@ package com.sokah.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //se ejecuta primero
+    private EditText username,password;
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.userNameInput);
+        password = findViewById(R.id.passwordInput);
+        loginButton = findViewById(R.id.loginbutton);
+
+        loginButton.setOnClickListener(
+                (v)->{
+
+                    String usernameText = username.getText().toString();
+                    String passwordText= password.getText().toString();
+                    Toast.makeText(this,usernameText +" "+ passwordText,Toast.LENGTH_LONG).show();
+                }
+        );
     }
 }
